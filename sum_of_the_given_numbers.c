80df48e0d914b8014da718c924bf2099397c6968
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-02-04 07:40:19 +0530

    #include<stdio.h> int main() {     int n1,n2,i,r;     scanf("%d",&r);     for(i=1; i<=r; i++)     {     scanf("%d %d",&n1,&n2);     printf("%d ",n1+n2);         }         return 0; }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n1,n2,i,r;
+    scanf("%d",&r);
+    for(i=1; i<=r; i++)
+    {
+    scanf("%d %d",&n1,&n2);
+    printf("%d
+",n1+n2);
+        }
+        return 0;
+}
