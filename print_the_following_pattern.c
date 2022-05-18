26b8eb5aa087301da9aa219d43592fc97acf1ead
Aman2913 <96662657+Aman2913@users.noreply.github.com>
2022-05-18 11:13:29 +0530

    #include <stdio.h>
    int main()
    {
    	int rows;
    scanf("%d", &rows);
    
    	for (int i = 1; i <= rows; i++)
    	{
    		for (int j = 1; j <= rows; j++)
    		{
    			if (i == j)
    			{
    				printf("0");
    			}
    			else
    			{
    				printf("x");
    			}
    		}
    		printf("
    ");
    	}
    }

---
+++ print_the_following_pattern.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	int rows;
+scanf("%d", &rows);
+
+	for (int i = 1; i <= rows; i++)
+	{
+		for (int j = 1; j <= rows; j++)
+		{
+			if (i == j)
+			{
+				printf("0");
+			}
+			else
+			{
+				printf("x");
+			}
+		}
+		printf("
+");
+	}
+}
